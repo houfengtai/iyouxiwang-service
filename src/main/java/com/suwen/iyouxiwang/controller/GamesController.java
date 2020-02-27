5223f24a4b60877a34c72259a90fbeb496647a18
houfengtai <houfengtai@sumawuliu.com>
2020-02-27 16:15:09 +0800

    init pro

--- src/main/java/com/suwen/iyouxiwang/controller/GamesController.java
+++ src/main/java/com/suwen/iyouxiwang/controller/GamesController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @RestController
-@RequestMapping(value = "/iyouxiwang")
+@RequestMapping(value = "/api/index")
 @Api("首页")
 public class GamesController extends BaseController{
     @Autowired
