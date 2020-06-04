01bbfbf16179f5225fd20a23a7acca85f414da05
houfengtai <houfengtai@sumawuliu.com>
2020-06-04 15:36:04 +0800

    init pro

--- src/main/java/com/suwen/iyouxiwang/controller/HomeController.java
+++ src/main/java/com/suwen/iyouxiwang/controller/HomeController.java
@@ -3,9 +3,11 @@ package com.suwen.iyouxiwang.controller;
 import com.suwen.framework.core.commons.annotation.IgnoreSecurity;
 import com.suwen.framework.core.commons.exception.CustomException;
 import com.suwen.framework.core.commons.resp.Response;
+import com.suwen.iyouxiwang.dao.GameAttachmentMapper;
 import com.suwen.iyouxiwang.dao.GamesMapper;
 import com.suwen.iyouxiwang.dao.HotGamesMapper;
 import com.suwen.iyouxiwang.dao.RecommendedGamesMapper;
+import com.suwen.iyouxiwang.domain.GameAttachmentView;
 import com.suwen.iyouxiwang.domain.GamesView;
 import com.suwen.iyouxiwang.service.GamesService;
 import io.swagger.annotations.Api;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -31,6 +34,8 @@ public class HomeController {
     private GamesService gamesService;
     @Autowired
     private GamesMapper gamesMapper;
+    @Autowired
+    private GameAttachmentMapper gameAttachmentMapper;
 
     @GetMapping("/hot/games")
     @ApiOperation(value = "查询首页火热游戏列表")
@@ -63,6 +68,16 @@ public class HomeController {
         Map<String, GamesView> map = new HashMap<>();
         if(qp_num!=0) map.put("qp", gamesMapper.selectByGameType(0,rand.nextInt(qp_num)));
         if(cq_num!=0) map.put("cq",gamesMapper.selectByGameType(1,rand.nextInt(cq_num)));
+        return Response.returnData(map);
+    }
+    @GetMapping("/games/view")
+    @ApiOperation(value = "查询首页游戏最新视频与最新图片")
+    @IgnoreSecurity
+    public Response<String> getImgAndVideo() throws CustomException {
+
+        Map<String, List<GameAttachmentView>> map = new HashMap<>();
+        map.put("images", gameAttachmentMapper.selectByType(0,0,3));
+        map.put("videos",gameAttachmentMapper.selectByType(1,0,3));
         return Response.returnData(map);
     }
 }
